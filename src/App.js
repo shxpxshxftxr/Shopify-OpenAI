9c6b85092e8a932f1c3a5de368b22867e45100f4
Cara Dodge <shxpxshxftxr@gmail.com>
2022-05-22 20:12:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import generatePrompt from "./api/haikus"
 
-function App() {
+export default function App() {
+  const key = process.env.REACT_APP_OPENAI_API_KEY;
+  const [promptInput, setPromptInput] = useState("");
+  const [results, setResults] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const aiAnswers = {
+      prompt: generatePrompt(promptInput),
+      temperature: .3,
+      max_tokens: 32,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    };
+    const response = await fetch(
+      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${key}`,
+        },
+        body: JSON.stringify(aiAnswers),
+      }
+    );
+    const data = await response.json();
+    setResults(data.choices[0].text);
+    setPromptInput("");
+    console.log("response:", data);
+  }
+
+  console.log("results:",results);
+  console.log("prompt:",promptInput);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1>Haiku AI</h1>
+        <h3>
+          Submit a theme to this AI and it will generate an original Haiku!
+        </h3>
       </header>
-    </div>
+      <label htmlFor="prompt">Enter Prompt Here:</label>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="prompt"
+          value={promptInput}
+          onChange={(event) => setPromptInput(event.target.value)}
+        />
+        <button type="submit">Submit Prompt</button>
+      </form>
+      <div>{results}</div>
+      {/* {Results.map((completion, index) => {
+        return (
+          <article className="results" key={index}>
+            <h5>Prompt: {completion[0]}</h5>
+            <h5>Haiku: {completion[1]}</h5>
+          </article>
+        );
+      })} */}
+    </main>
   );
 }
-
-export default App;
---
+++ src/api/haikus.js
@@ -0,0 +1,19 @@
+
+export default function generatePrompt(theme) {
+  const capitalizedTheme =
+    theme[0].toUpperCase() + theme.slice(1).toLowerCase();
+  return `A Haiku is a poem of three lines. There are five syllables in the first line, seven in the second, and five in the last. Write a Haiku.
+
+  Theme: Frog
+  Haiku:
+  The silent old pond
+  a mirror of ancient calm,
+  a frog-leaps-in splash.
+  Theme: Preciousness
+  Haiku:
+  Dew evaporates,
+  and our world is dew... so dear,
+  so fresh ...so fleeting.
+  Theme: ${capitalizedTheme}
+  Haiku:`;
+}
